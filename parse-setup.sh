a2e1926ba8077b84c9864702251802e36d995e7c
Igor <xdesigns@gmail.com>
2016-04-25 22:53:38 -0400

    minor license copy change

--- parse-setup.sh
+++ parse-setup.sh
@@ -139,7 +139,7 @@ echo -p "Do you have everything you need to start? (y/n)?"
 					
 					echo "- Porting NGINX and MongoDb SSL Licence. -"
 					sleep 2
-					sudo cat /etc/letsencrypt/archive/$input/{fullchain1.pem,privkey1.pem} | sudo tee /etc/ssl/mongo.pem
+					sudo cat /etc/letsencrypt/archive/$domain/{fullchain1.pem,privkey1.pem} | sudo tee /etc/ssl/mongo.pem
 					sudo chown mongodb:mongodb /etc/ssl/mongo.pem
 					sudo chmod 600 /etc/ssl/mongo.pem
 
